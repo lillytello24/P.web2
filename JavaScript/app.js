eff8e1868aaa9ceba1a11ba435d2bdba08f35273
lili con i <136383335+lillytello24@users.noreply.github.com>
2024-08-26 23:18:36 -0600

    Add files via upload
    Mi programa javaScript

---
+++ JavaScript/app.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Ingrese nombre:");
+let apellido = prompt("Ingrese apellido:");
+let edad = prompt("Ingrese su edad:");
+let peso = prompt("Ingrese su peso en kg:");
+let estatura = prompt("Ingrese su estatura en metros:");
+let imc = parseFloat(peso)/parseFloat(estatura*estatura);
+
+console.log("Hola: " + nombre, "Tienes " + edad + " años", "Tu IMC es:" + imc)
